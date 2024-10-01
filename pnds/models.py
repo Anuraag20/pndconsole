13436efbf6ff07dcb16367bcdea6f5693828f51d
Anuraag <anuraagmukherjee1@gmail.com>
2024-10-01 02:48:38 +0100

    Created model for keeping a track of scheduled pumps

--- pnds/models.py
+++ pnds/models.py
@@ -10,7 +10,31 @@ from market.models import (
 
 class ScheduledPump(models.Model):
 
-    message = models.ForeignKey(Message)
+    message = models.ForeignKey(Message, on_delete = models.CASCADE)
+
+    pair = models.ForeignKey(Coin, on_delete = models.CASCADE, null = True, blank = True, related_name = 'pump_pair')
+    target = models.ForeignKey(Coin, on_delete = models.CASCADE, null = True, blank = True, related_name = 'pump_target')
+    scheduled_at = models.DateTimeField(null = True, blank = True)
+    
+    notes = models.TextField(null = True, blank = True)
+
+    false_alarm = models.BooleanField(default = False)
+    pair_guessed = models.BooleanField(default = False)
+    manually_corrected = models.BooleanField(default = False)
+
+    def save(self, *args, **kwargs):
+        
+        if self.false_alarm:
+            self.manually_corrected = True
+
+        return super(ScheduledPump, self).save(*args, **kwargs)
+    
+
+
+    
+
+            
+
 
 
 
